9fc635154ad76d8c1916490b746074e29d0da6f5
km-witch <kmkim@witchcompany.io>
2023-03-29 15:48:15 +0900

    0.0.10
    - 페인팅 스크립트 변경

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/Modules/InteractionAssets/WitchPaintableBrushChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace WitchCompany.Toolkit.Module
+{
+    public class WitchPaintableBrushChanger : WitchBehaviour
+    {
+        public override string BehaviourName => "페인트 브러쉬";
+
+        public override string Description => "클릭시 페인트 브러쉬 색상을 변경해주는 오브젝트 입니다.";
+
+        public override string DocumentURL => throw new System.NotImplementedException();
+
+        [Header("변경할 색상")] 
+        public Color color;
+
+        [Header("클릭 콜라이더")] 
+        public Collider clickCollider;
+    }
+}
