cd2f96b4087f0b4170da94ac487f190a960d3212
km-witch <kmkim@witchcompany.io>
2023-04-04 16:45:56 +0900

    페인트 벽 제한 변경
     [RequireComponent(typeof(MeshCollider), typeof(MeshRenderer))]

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchPaintableWall.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchPaintableWall.cs
@@ -5,19 +5,18 @@ using WitchCompany.Toolkit.Validation;
 
 namespace WitchCompany.Toolkit.Module
 {
+    [RequireComponent(typeof(MeshCollider), typeof(MeshRenderer))]
     public class WitchPaintableWall : WitchBehaviourUnique
     {
         public override string BehaviourName => "낙서 벽";
         public override string Description => "유저가 자유롭게 낙서할 수 있는 벽입니다.\n" +
+                                              "매쉬콜라이더 및 매쉬렌더러가 필요합니다.\n" +
                                               "낙서가 되는 벽면의 텍스쳐 및 색상은 낙서 설정을 따릅니다.\n" +
                                               "브러쉬가 없다면 검은색, 있다면 첫브러쉬의 색상으로 시작합니다.";
         public override string DocumentURL => "";
 
         public override int MaximumCount => 1;
 
-        [Header("낙서가 그려질 렌더러"), SerializeField] 
-        private Renderer targetRenderer;
-        
         [Header("브러쉬 리스트"), SerializeReference] 
         private List<WitchPaintableBrush> brushes;
 
@@ -25,7 +24,7 @@ namespace WitchCompany.Toolkit.Module
         [SerializeField] private Ratio paintRatioX = Ratio._512;
         [SerializeField] private Ratio paintRatioY = Ratio._512;
         
-        [Header("텍스쳐 설정")]
+        [Header("기본 텍스쳐 설정")]
         [SerializeField] private Texture2D baseTexture;
         [SerializeField] private Color baseColor = Color.white;
 
@@ -37,7 +36,6 @@ namespace WitchCompany.Toolkit.Module
             _1024 = 1024
         }
 
-        public Renderer TargetRenderer => targetRenderer;
         public List<WitchPaintableBrush> Brushes => brushes;
         public Vector2Int PaintRatio => new((int)paintRatioX, (int)paintRatioY);
         public Texture2D BaseTex => baseTexture;
@@ -46,8 +44,8 @@ namespace WitchCompany.Toolkit.Module
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
         {
-            if (targetRenderer == null) return NullError(nameof(targetRenderer));
-            if (!transform.HasChild(targetRenderer)) return ChildError(nameof(targetRenderer));
+            if (TryGetComponent(out MeshCollider _)) return NullError(nameof(MeshCollider));
+            if (TryGetComponent(out MeshRenderer _)) return NullError(nameof(MeshRenderer));
 
             if (TryGetComponent<WitchPaintableBrush>(out var b))
                 return Error($"{b.BehaviourName}는 {BehaviourName}의 자식이어야 합니다.");
