556f306e4ba9c5c30085d95f9505f60104b0a612
witch-kang <jhkang@witchcompany.io>
2023-04-06 12:21:59 +0900

    witchProductDisplay 컴포넌트에서 안뜨는 오류 해결
    클래스명과 파일명이 달라서 생긴 오류

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchProductDisplay.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchProductDisplay.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 using WitchCompany.Toolkit.Module;
 using WitchCompany.Toolkit.Validation;
 
-namespace WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Display
+namespace WitchCompany.Toolkit.Module
 {
-    public class WitchProductUpload : WitchBehaviour
+    public class WitchProductDisplay : WitchBehaviour
     {
         public override string BehaviourName => "전시: 상품";
         public override string Description => "상품 ID를 입력해 상품을 등록할 수 있습니다.\n" +
