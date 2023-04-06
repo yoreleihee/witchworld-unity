f2a8fa93c65227d314d85e0928900ebfaf68bb4a
km-witch <kmkim@witchcompany.io>
2023-04-06 17:28:53 +0900

    Update StaticRevertTool.cs

--- packages/io.witchcompany.toolkit/Editor/Scripts/Tool/StaticRevertTool.cs
+++ packages/io.witchcompany.toolkit/Editor/Scripts/Tool/StaticRevertTool.cs
@@ -18,6 +18,7 @@ namespace WitchCompany.Toolkit.Editor.Tool
             // staticObjects Null 체크 후 새로 생성, 혹은 비우기
             _staticObjects ??= new Dictionary<int, StaticEditorFlags>();
             _staticObjects.Clear();
+            Debug.Log("1. static 초기화");
 
             // 모은 오브젝트를 순회하며,
             foreach (var tr in GetAllTransforms())
@@ -31,12 +32,15 @@ namespace WitchCompany.Toolkit.Editor.Tool
                 var id = ObjectTool.GetLocalIdentifier(go);
                 // 해당 오브젝트의 static flag 가져오기
                 var flag = GameObjectUtility.GetStaticEditorFlags(go);
+                Debug.Log($"2. 오브젝트 추가: {go.name}({id}): {flag}");
 
                 // 캐싱
                 _staticObjects.Add(id, flag);
                 // static flag 해제
                 GameObjectUtility.SetStaticEditorFlags(go, 0);
             }
+            
+            Debug.Log($"3. static 해제 성공");
         }
         
         public static void RevertFlags()
@@ -44,6 +48,7 @@ namespace WitchCompany.Toolkit.Editor.Tool
             // staticObjects가 없으면 종료
             if(_staticObjects is not {Count: > 0}) return;
             
+            Debug.Log($"4. static 복구 시작");
             // 모은 오브젝트를 순회하며,
             foreach (var tr in GetAllTransforms())
             {
@@ -57,8 +62,11 @@ namespace WitchCompany.Toolkit.Editor.Tool
                 {
                     // static flag 되돌리기
                     GameObjectUtility.SetStaticEditorFlags(go, flag);
+                    Debug.Log($"5. 오브젝트 돌리기: {go.name}({id}): {flag}");
                 }
             }
+            
+            Debug.Log($"6. static 복구 종료");
         }
 
         // 현재 씬의 모든 오브젝트 가져오기
