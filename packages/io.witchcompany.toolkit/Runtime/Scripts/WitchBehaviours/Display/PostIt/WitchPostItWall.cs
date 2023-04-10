1426f43be13548e3a247f70d9683466e0b94b1b7
km-witch <kmkim@witchcompany.io>
2023-04-10 09:59:35 +0900

    PostIt recap

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/PostIt/WitchPostItWall.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/PostIt/WitchPostItWall.cs
@@ -3,25 +3,40 @@ using WitchCompany.Toolkit.Validation;
 
 namespace WitchCompany.Toolkit.Module
 {
-    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(Canvas))]
     public class WitchPostItWall : WitchBehaviourUnique
     {
         public override string BehaviourName => "전시: 포스트잇 벽";
 
         public override string Description => "포스트잇을 붙일 수 있는 벽입니다.\n" +
-                                              "IsTrigger가 체크되지 않은 박스 콜라이더가 필요합니다.\n" +
+                                              "World Space 캔버스가 필요합니다.\n" +
                                               @"'전시: 포스트잇 생성 버튼'이 씬에 있어야 합니다.";
 
         public override string DocumentURL => "";
         public override int MaximumCount => 1;
         
 #if UNITY_EDITOR
+        private void Reset()
+        {
+            if (!TryGetComponent(out Canvas canvas)) 
+                canvas = gameObject.AddComponent<Canvas>();
+            
+            canvas.renderMode = RenderMode.WorldSpace;
+            canvas.sortingLayerName = "Default";
+            canvas.sortingOrder = 0;
+    
+            transform.position = Vector3.zero;
+            transform.localScale = Vector3.one * 0.01f;
+        }
+
         public override ValidationError ValidationCheck()
         {
-            if (!TryGetComponent(out BoxCollider col))
-                return NullError(nameof(BoxCollider));
-            if (col.isTrigger)
-                return Error("콜라이더는 IsTrigger 체크될 수 없습니다.");
+            if (!TryGetComponent(out Canvas canvas))
+                return NullError(nameof(Canvas));
+            if (canvas.renderMode != RenderMode.WorldSpace)
+                return Error("캔버스는 worldSpace여야 합니다.");
+            if (Vector3.Distance(transform.localScale,  Vector3.one * 0.01f) > 0.001f)
+                return Error("캔버스의 스케일은 0.01이어야 합니다.");
             if (FindObjectOfType<WitchPostItCreator>() == null)
                 return Error("'전시: 포스트잇 생성 버튼'이 씬에 있어야 합니다. 생성해주세요.");
 
