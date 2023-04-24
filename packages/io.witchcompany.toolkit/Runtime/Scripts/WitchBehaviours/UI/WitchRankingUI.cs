dd350d66af3f72db36c92274d96127605b6e23ae
witch-kang <jhkang@witchcompany.io>
2023-04-24 15:18:52 +0900

    랭킹 텍스트 동기화

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchRankingUI.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchRankingUI.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 using WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Event.Base;
 using WitchCompany.Toolkit.Validation;
 
@@ -14,15 +16,30 @@ namespace WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.UI
         [Header("랭킹에 표시할 값의 key"), SerializeField]
         private string[] keys;
         [Header("랭킹을 표시할 게임 오브젝트"), SerializeField] 
-        private WitchSyncedTextHandlerUI rankingPrefab;
+        private WitchSyncedTextHandlerUI rankingCard;
+        [Header("랭킹을 표시할 위치"), SerializeField] 
+        private Transform content;
         [Header("첫 화면에 보여줄 페이지"), SerializeField]
         private int page;
         [Header("한 페이지에 표시할 랭킹 개수"), SerializeField]
         private int limit;
         [Header("순위 표시 여부"), SerializeField]
         private bool isRanking;
+
+        [HideInInspector] public UnityEvent show = new();
+        [HideInInspector] public UnityEvent hide = new();
         
+        public void Show() => show.Invoke();
+        public void Hide() => hide.Invoke();
+
+        public string[] Keys => keys;
+        public WitchSyncedTextHandlerUI RankingCard => rankingCard;
+        public Transform Content => content;
+        public int Page => page;
+        public int Limit => limit;
+        public bool IsRanking => isRanking;
         
+
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
         {
--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchSyncedTextHandlerUI.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchSyncedTextHandlerUI.cs
@@ -8,7 +8,9 @@ namespace WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.UI
         public override string BehaviourName => "UI: 텍스트 동기화 핸들러";
         public override string Description => "자식 오브젝트가 WitchSyncedTextUI 컴포넌트를 가지고 있다면 type에 맞는 값을 Text Mesh Pro의 text로 지정합니다.";
         public override string DocumentURL => "";
-        
+
+        [Header("랭킹"), SerializeField]
+        private WitchRankingUI ranking;
         
     }
 }
