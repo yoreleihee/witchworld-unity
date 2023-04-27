dab326ddd4525fbd2a43bfce7514f3cd9e1ad3a7
km-witch <kmkim@witchcompany.io>
2023-04-27 21:42:33 +0900

    chair

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchChair.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using WitchCompany.Toolkit.Extension;
+using WitchCompany.Toolkit.Validation;
+
+namespace WitchCompany.Toolkit.Module
+{
+    public class WitchChair : WitchBehaviourUnique
+    {
+        public override string BehaviourName => "의자";
+        public override string Description => "의자입니다.";
+        public override string DocumentURL => "";
+
+        [Header("플레이어가 앉을 위치")] 
+        [SerializeField] private Transform sitTarget;
+
+        public Transform SitTarget => sitTarget;
+        
+#if UNITY_EDITOR
+        public override ValidationError ValidationCheck()
+        {
+            if (sitTarget == null) return NullError("sitTarget");
+            if (!transform.HasChild(sitTarget, true)) return ChildError("sitTarget");
+            
+            return null;
+        }
+#endif
+    }
+}
