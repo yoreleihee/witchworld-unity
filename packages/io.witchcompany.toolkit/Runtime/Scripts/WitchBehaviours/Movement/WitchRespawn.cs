c721fc90af0c46080719647b3266acc5950c2ea3
witch-kang <jhkang@witchcompany.io>
2023-05-04 15:24:55 +0900

    리스폰 툴킷 추가

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Movement/WitchRespawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using WitchCompany.Toolkit.Attribute;
+using WitchCompany.Toolkit.Runtime.Base;
+using WitchCompany.Toolkit.Validation;
+
+namespace WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Event
+{
+    public class WitchRespawn : WitchMovementBase
+    {
+        public override string BehaviourName => "이동: 리스폰";
+        public override string Description => "플레이어의 위치를 스폰 포인트로 이동시키는 요소입니다.\n" +
+                                              "Y축 제한 여부를 활성화하면 플레이어의 Y축 위치가 제한값 이하일 때 리스폰됩니다.\n"+
+                                              "Y축 제한 설정을 하지 않으면 오브젝트에 isTrigger가 활성화 된 Collider가 필요합니다.";
+        public override string DocumentURL => "";
+
+        [Header("Y축 제한 여부"), SerializeField]
+        private bool isLimitY;
+        [Header("Y축 제한값"), SerializeField]
+        private float limitY;
+
+        public bool IsLimitY => isLimitY;
+        public float LimitY => limitY;
+        
+        
+#if UNITY_EDITOR
+        public override ValidationError ValidationCheck()
+        {
+            if (!TryGetComponent(out Collider col) && !isLimitY)
+                return NullError("Collider");
+
+            if (!col.isTrigger) return TriggerError(col);
+            
+            return base.ValidationCheck();
+        }
+#endif 
+    }
+}
--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchTimerUI.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchTimerUI.cs
@@ -1,14 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Event.Base;
-using WitchCompany.Toolkit.Validation;
 
 
 namespace WitchCompany.Toolkit.Module.PhysicsEffect
 {
-    public class WitchTimer : WitchUIBase
+    public class WitchTimerUI : WitchUIBase
     {
-        public override string BehaviourName => "효과: 타이머";
+        public override string BehaviourName => "UI: 타이머";
         public override string Description => "타이머를 시작, 종료할 수 있는 요소입니다.\n" +
                                               "TextMeshPro - Text (UI)가 있는 오브젝트에 부착해야 합니다.\n"+
                                               "mm:ss:ms 로 표시됩니다.";
