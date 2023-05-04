972b086e7417a5c162eee6d184f01cbdf1debacc
witch-kang <jhkang@witchcompany.io>
2023-05-04 16:48:10 +0900

    api url prod 임시 변경

--- packages/io.witchcompany.toolkit/Editor/Scripts/Configs/ApiConfig.cs
+++ packages/io.witchcompany.toolkit/Editor/Scripts/Configs/ApiConfig.cs
@@ -6,7 +6,7 @@ namespace WitchCompany.Toolkit.Editor.Configs
     {
         /// <summary>서버 URL 가져오기</summary>
         public static string URL(string url) => 
-            "https://dev-go.witchworld.io/services/api/" + url;
+            "https://prod-go.witchworld.io/services/api/" + url;
 
         /// <summary>토큰 헤더 가져오기</summary>
         public static Dictionary<string, string> TokenHeader(string token) =>
