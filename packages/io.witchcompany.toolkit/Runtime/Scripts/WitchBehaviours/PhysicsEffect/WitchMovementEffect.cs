6c45f27a86df5160a058f9a2675baafeb3766f92
witch-kang <jhkang@witchcompany.io>
2023-05-09 15:33:56 +0900

    두 지점 이동 추가

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/PhysicsEffect/WitchMovementEffect.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/PhysicsEffect/WitchMovementEffect.cs
@@ -2,13 +2,15 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 using WitchCompany.Core;
+using WitchCompany.Toolkit.Validation;
 
 namespace WitchCompany.Toolkit.Module.PhysicsEffect
 {
     public class WitchMovementEffect : WitchPhysicsEffectBase
     {
-        public override string BehaviourName => "물리효과: 이동";
-        public override string Description => "오브젝트를 이동시킬 수 있는 요소입니다.";
+        public override string BehaviourName => "물리효과: 두 지점 사이 이동";
+        public override string Description => "오브젝트를 왕복 이동시킬 수 있는 요소입니다.\n" +
+                                              "시작 지점과 끝 지점이 필요합니다.";
         public override string DocumentURL => "";
 
 
@@ -16,36 +18,34 @@ namespace WitchCompany.Toolkit.Module.PhysicsEffect
         {
             Constant,
             EaseIn,
-            EaseOut
+            EaseOut,
+            EaseInOut
         }
-
-        [Header("이동할 위치"), SerializeField]
-        private Transform target;
+        [Header("시작 지점"), SerializeField]
+        private Transform startPos;
+        [Header("끝 지점"), SerializeField]
+        private Transform endPos;
         [Header("동작"), SerializeField]
-        private CurveType moveType;
+        private CurveType type;
         [Header("시간"), SerializeField]
-        private float moveTime;
-        // [Header("진폭"), SerializeField]
-        // private float amplitude;
+        private int moveTime;
 
-        public Transform TargetPos => target;
-        public float MoveTime => moveTime;
-        public CurveType Curve => moveType;
-        public float Len => Vector3.Magnitude(target.position - transform.position); 
+        public Transform StartTr => startPos;
+        public Transform EndTr => endPos;
+        public int MoveTime => moveTime;
+        public CurveType Curve => type;
+        public float Distance => Vector3.Magnitude(endPos.position - startPos.position);
 
-        public AnimationCurve GetCurve(CurveType type)
+#if UNITY_EDITOR
+        public override ValidationError ValidationCheck()
         {
-            switch (type)
-            {
-                case CurveType.Constant:
-                    return AnimationCurve.Linear(0, 0, moveTime, Len);
-                case CurveType.EaseIn:
-                    return AnimationCurve.EaseInOut(0, 0, moveTime, Len);
-                case CurveType.EaseOut:
-                    return AnimationCurve.EaseInOut(0, Len, moveTime, 0);
-                default:
-                    throw new ArgumentException($"정의되지 않은 MoveType : {type}");
-            }
+            if (!startPos)
+                return NullError("startPos");
+            if (!endPos)
+                return NullError("targetPos");
+            
+            return base.ValidationCheck();
         }
+#endif
     }
 }
