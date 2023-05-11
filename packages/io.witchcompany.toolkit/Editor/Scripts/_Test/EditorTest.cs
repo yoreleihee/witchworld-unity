60ab72cb15c46e84f5c072876d097486be04c03e
witch-kang <jhkang@witchcompany.io>
2023-05-11 18:23:44 +0900

    블록 아이디 조회 메뉴 추가 (임시)

--- packages/io.witchcompany.toolkit/Editor/Scripts/_Test/EditorTest.cs
+++ packages/io.witchcompany.toolkit/Editor/Scripts/_Test/EditorTest.cs
@@ -1,21 +1,20 @@
-﻿using Unity.Plastic.Newtonsoft.Json;
+﻿using Cysharp.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 using WitchCompany.Toolkit.Editor.API;
 using WitchCompany.Toolkit.Editor.Configs;
-using WitchCompany.Toolkit.Editor.DataStructure;
-using WitchCompany.Toolkit.Editor.Tool;
-using Formatting = Newtonsoft.Json.Formatting;
-using JsonConvert = Newtonsoft.Json.JsonConvert;
 
 namespace WitchCompany.Toolkit.Editor
 {
     public static class EditorTest
     {
-        // [MenuItem("WitchToolkit/View Auth")]
-        // public static void GetAuth()
-        // {
-        //     Debug.Log(AuthConfig.Auth.accessToken);
-        // }
+        [MenuItem("WitchToolkit/Get BlockId")]
+        public static async void GetBlockId()
+        {
+            var result = await WitchAPI.GetBlock(AdminConfig.PathName);
+
+            var mode = ToolkitConfig.DeveloperMode ? "Dev" : "Prod";
+            Debug.Log($"{mode}의 blockId : {result.blockId}");
+        }
     }
 }
