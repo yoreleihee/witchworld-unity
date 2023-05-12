6ed83843fbf4a0bcf87039e02eef1741bd61c173
yhlin-witch <132861870+yhlin-witch@users.noreply.github.com>
2023-05-12 13:36:22 +0900

    카메라 바라보기 툴킷 추가

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/UI/WitchFaceCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Event.Base;
+using WitchCompany.Toolkit.Validation;
+
+namespace WitchCompany.Toolkit.Module.PhysicsEffect
+{
+    public class WitchFaceCamera : WitchUIBase
+    {
+        public override string BehaviourName => "UI: 카메라 방향 보기";
+        public override string Description => "UI가 카메라 방향을 바라보게 만듭니다.";
+        public override string DocumentURL => "";
+
+        
+        [Header("UI Transform"), SerializeField]
+        private Transform uiTransform;
+        
+        public Transform UITransform => uiTransform;
+        
+#if UNITY_EDITOR
+        public override ValidationError ValidationCheck()
+        {
+            return null;
+        }
+#endif
+    }
+}
