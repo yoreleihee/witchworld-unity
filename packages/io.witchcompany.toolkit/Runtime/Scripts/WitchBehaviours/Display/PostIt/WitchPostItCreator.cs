6f76472b3a8acc67ebce46d0b3be4bf05f4569b0
yhlin-witch <132861870+yhlin-witch@users.noreply.github.com>
2023-05-12 14:05:02 +0900

    카메라 바라보기, 포스트잇 일부 수정

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/PostIt/WitchPostItCreator.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/PostIt/WitchPostItCreator.cs
@@ -7,7 +7,7 @@ namespace WitchCompany.Toolkit.Module
     public class WitchPostItCreator : WitchBehaviourUnique
     {
         public override string BehaviourName => "전시: 포스트잇 생성 버튼";
-        public override string Description => "포스트잇을 붙일 수 있는 벽입니다.\n" +
+        public override string Description => "포스트잇을 생성할 수 있는 요소입니다.\n" +
                                               "콜라이더가 필요합니다.\n" +
                                               "'전시: 포스트잇 벽'이 씬에 있어야 합니다.";
         public override string DocumentURL => "";
--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Effect/WitchFaceCamera.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Effect/WitchFaceCamera.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 using WitchCompany.Toolkit.Runtime.Scripts.WitchBehaviours.Event.Base;
 using WitchCompany.Toolkit.Validation;
 
@@ -6,15 +7,15 @@ namespace WitchCompany.Toolkit.Module.PhysicsEffect
 {
     public class WitchFaceCamera : WitchUIBase
     {
-        public override string BehaviourName => "UI: 카메라 방향 보기";
-        public override string Description => "UI가 카메라 방향을 바라보게 만듭니다.";
+        public override string BehaviourName => "효과: 카메라 방향 보기";
+        public override string Description => "오브젝트가 카메라 방향을 바라보게 만듭니다.";
         public override string DocumentURL => "";
 
         
-        [Header("UI Transform"), SerializeField]
-        private Transform uiTransform;
+        [FormerlySerializedAs("Transform")] [Header("Transform"), SerializeField]
+        private Transform objectTransform;
         
-        public Transform UITransform => uiTransform;
+        public Transform ObjTransform => objectTransform;
         
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
