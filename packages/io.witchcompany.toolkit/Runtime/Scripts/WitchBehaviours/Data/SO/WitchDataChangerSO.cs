a1878f0aa7196fb90aa48d1ed395a05ebb999a95
km-witch <kmkim@witchcompany.io>
2023-06-05 10:36:52 +0900

    Revert "dataChangerSO add bool saveOnServer"
    This reverts commit d140d098bba92445f42bcf5aadfdbbdbf403c628.

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Data/SO/WitchDataChangerSO.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Data/SO/WitchDataChangerSO.cs
@@ -12,9 +12,6 @@ namespace WitchCompany.Toolkit.Module
         [Header("연산할 값")] 
         public ArithmeticOperator valueOperator = ArithmeticOperator.Assignment;
         public int value = 0;
-        
-        [Header("서버에 쓸지?")] 
-        public bool saveOnServer = true;
 
         [HideInInspector] public UnityEvent<WitchDataChangerSO> onEvent = new();
         
