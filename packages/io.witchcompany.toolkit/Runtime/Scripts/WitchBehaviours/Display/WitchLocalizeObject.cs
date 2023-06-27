116b3e1b4f53f6a08ccde29174befb9a57ce6777
P-Seongbeom <sbpark@witchcompany.io>
2023-06-27 19:51:32 +0900

    localize object
    localize object

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchLocalizeObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WitchCompany.Toolkit.Module;
+
+namespace WitchCompany.Toolkit
+{
+    public class WitchLocalizeObject : WitchBehaviour
+    {
+        public override string BehaviourName => "로컬라이즈 3D 오브젝트";
+
+        public override string Description => "로컬라이즈가 필요한 3D 오브젝트에 필요한 요소입니다." + 
+                                              "한국어/영어 버전의 3D 오브젝트가 필요합니다.";
+        public override string DocumentURL => "";
+        
+        [field: Header("3D 오브젝트")]
+        [field: SerializeField] public GameObject KoObject { get; private set; }
+        [field: SerializeField] public GameObject EnObject { get; private set; }
+    }
+}
