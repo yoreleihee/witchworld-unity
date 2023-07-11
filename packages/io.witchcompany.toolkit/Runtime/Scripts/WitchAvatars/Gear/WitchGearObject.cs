1a08385569c9225d5061d5422543d690ff97cef7
witch-kang <jhkang@witchcompany.io>
2023-07-11 15:28:11 +0900

    WitchGearObject Height 옵션 추가

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchAvatars/Gear/WitchGearObject.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchAvatars/Gear/WitchGearObject.cs
@@ -8,9 +8,12 @@ namespace WitchCompany.Toolkit
         [SerializeField] private GearType gearType;
         [Header("비활성화 할 신체 부위")]
         [SerializeField] private SkinType skinType;
+        [Header("아바타의 신장(m)"), Range(0.5f, 3f)]
+        [SerializeField] private float height = 1.5f;
         
         public GearType GearType => gearType;
         public SkinType SkinType => skinType;
+        public float Height => height;
 
 #if UNITY_EDITOR
 
