3d3b2f2e8cdb29a6e17d57a6ad5dc0076ddac78c
HuiJiChoi <hjchoi@witchcompany.io>
2023-09-13 19:02:16 +0900

    부스 간 거리가 가까울 시 Validation error 발생

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchBooth.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchBooth.cs
@@ -7,8 +7,7 @@ namespace WitchCompany.Toolkit.Module
     public class WitchBooth : WitchBehaviourUnique
     {
         public override string BehaviourName => "전시: 부스";
-        public override string Description => "구걸/경매를 할 수 있는 부스입니다.";
-
+        public override string Description => "구걸/경매를 할 수 있는 부스입니다. 반경 10m 이내에 타 오브젝트가 있으면 겹쳐보일 수 있습니다.";
         public override string DocumentURL => "https://www.notion.so/witchcompany/WitchBooth-b4daac7b312f47f2b3167e8c76fb64af?pvs=4";
         public override int MaximumCount => 20;
         
@@ -18,26 +17,27 @@ namespace WitchCompany.Toolkit.Module
         public BoothType BoothType => boothType;
 
 #if UNITY_EDITOR
-
-        [SerializeField]
-        private float radius = 10f;        
-        public override ValidationError ValidationCheck()
-        {
-            var colliders = Physics.OverlapSphere(transform.position, radius);
-            foreach (var col in colliders)
-            {
-                if (col.name == "Plane") continue;
-                if (col != null) return DistanceError(col);
-            }
-            
-            return null;
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(this.transform.position, radius);
-        }
+        
+//         타 오브젝트의 콜라이더와 겹치지 않게
+//         [SerializeField]
+//         private float radius = 10f;        
+//         public override ValidationError ValidationCheck()
+//         {
+//             var colliders = Physics.OverlapSphere(transform.position, radius);
+//             foreach (var col in colliders)
+//             {
+//                 if (col.name == "Plane") continue;
+//                 if (col != null) return DistanceError(col);
+//             }
+//             
+//             return null;
+//         }
+//
+//         private void OnDrawGizmos()
+//         {
+//             Gizmos.color = Color.green;
+//             Gizmos.DrawWireSphere(this.transform.position, radius);
+//         }
 #endif
     }
 }
