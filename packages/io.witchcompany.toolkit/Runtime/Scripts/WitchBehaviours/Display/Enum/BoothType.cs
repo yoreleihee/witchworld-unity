568639cece11e99af6c220cc5906ec0b53099b3b
HuiJiChoi <hjchoi@witchcompany.io>
2023-09-13 11:53:46 +0900

    부스 툴킷 스크립트, 부스 타입 Enum 스크립트 작성

---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/Enum/BoothType.cs
@@ -0,0 +1,8 @@
+namespace WitchCompany.Toolkit.Module
+{
+    public enum BoothType
+    {
+        Auction,
+        Begging
+    }
+}
---
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchBooth.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace WitchCompany.Toolkit.Module
+{
+    public class WitchBooth : WitchBehaviourUnique
+    {
+        public override string BehaviourName => "전시: 부스";
+        public override string Description => "구걸/경매를 할 수 있는 부스입니다.";
+
+        public override string DocumentURL => "https://www.notion.so/witchcompany/WitchBooth-b4daac7b312f47f2b3167e8c76fb64af?pvs=4";
+        public override int MaximumCount => 20;
+        
+        [Header("부스 타입"), SerializeField]
+        private BoothType boothType;
+
+        public BoothType BoothType => boothType;
+    }
+}
