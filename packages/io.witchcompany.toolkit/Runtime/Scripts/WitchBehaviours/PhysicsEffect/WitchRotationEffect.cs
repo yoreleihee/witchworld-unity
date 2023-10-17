aa0fa0c2f2c7e64af15379491b3c5453e11b34db
km-witch <kmkim@witchcompany.io>
2023-10-17 11:22:36 +0900

    0.2.20
    오브젝트 회전 기능 업데이트

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/PhysicsEffect/WitchRotationEffect.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/PhysicsEffect/WitchRotationEffect.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using WitchCompany.Toolkit.Attribute;
 
 namespace WitchCompany.Toolkit.Module.PhysicsEffect
 {
@@ -9,12 +10,45 @@ namespace WitchCompany.Toolkit.Module.PhysicsEffect
         public override string Description => "오브젝트에 회전하는 효과를 줍니다.";
         public override string DocumentURL => "https://www.notion.so/witchcompany/WitchRotationEffect-1ab98bb5a6ed4cec85ee7bffd430121c?pvs=4";
         public override int MaximumCount => 64;
+
+        public enum Mode
+        {
+            Speed = 0,
+            Destination
+        }
+
+        public enum EaseType
+        {
+            Linear = 0,
+            EaseIn,
+            EaseOut,
+            EaseInOut
+        }
+
+        [Header("모드")]
+        [SerializeField] private Mode rotationMode = Mode.Speed;
         
-        [Header("회전 속도")] 
+        [Space(15)]
+        
+        [ShowIf(nameof(rotationMode), Mode.Speed)][Tooltip("회전 속도 X")]
         [Range(-30f, 30f), SerializeField] private float speedX = 0f;
+        [ShowIf(nameof(rotationMode), Mode.Speed)][Tooltip("회전 속도 Y")]
         [Range(-30f, 30f), SerializeField] private float speedY = 0.5f;
+        [ShowIf(nameof(rotationMode), Mode.Speed)][Tooltip("회전 속도 Z")]
         [Range(-30f, 30f), SerializeField] private float speedZ = 0;
 
+        [ShowIf(nameof(rotationMode), Mode.Destination)] [Tooltip("목표 회전 값")]
+        [SerializeField] private Vector3 endValue;
+        [ShowIf(nameof(rotationMode), Mode.Destination)] [Tooltip("목표 회전 시간")] 
+        [SerializeField, Range(0.1f, 300f)] private float duration = 1f;
+        [ShowIf(nameof(rotationMode), Mode.Destination)] [Tooltip("회전 보간 타입")]
+        [SerializeField] private EaseType ease = EaseType.Linear;
+
+        public Mode RotationMode => rotationMode;
         public Vector3 Speed => new(speedX, speedY, speedZ);
+        
+        public Vector3 EndValue => endValue;
+        public float Duration => duration;
+        public EaseType Ease => ease;
     }
 }
