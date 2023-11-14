7d3b271a643b519687d847edaf933e31a7dde448
HuiJiChoi <hjchoi@witchcompany.io>
2023-11-14 12:20:02 +0900

    WitchGoodsDisplay 추가

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchGoodsDisplay.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchGoodsDisplay.cs
@@ -8,7 +8,8 @@ namespace WitchCompany.Toolkit.Module
     {
         public override string BehaviourName => "전시: 지정 상품";
         public override string Description => "판매 상품 ID를 입력해 구매한 상품을 전시할 수 있습니다.\n" +
-                                              "오브젝트에 Collider가 있어야 합니다.";
+                                              "루트 오브젝트에 툴킷을 부착하고 자식 오브젝트로 전시 에셋과 전시대 오브젝트를 설정해주세요\n" +
+                                              "전시 에셋과 전시대 오브젝트에는 Collider가 있어야 합니다.";
         public override string DocumentURL => "";
 
         [Header("판매 상품 ID")] 
@@ -20,11 +21,24 @@ namespace WitchCompany.Toolkit.Module
         
         [field: Header("구매한 아이템 전시 이벤트")]
         [field: SerializeField] public UnityEvent OnDisplayEvent { get; private set; }
+        
+        [field: Header("전시 에셋")]
+        [field: SerializeField] public GameObject DisplayAsset { get; private set; }
+        
+        [field: Header("전시대")]
+        [field: SerializeField] public GameObject DisplayShelf { get; private set; }
 
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
         {
-            if (!TryGetComponent(out Collider _)) return NullError("Collider");
+            if (!DisplayAsset.TryGetComponent(out Collider _))
+                return new ValidationError("전시 에셋에 Collider가 있어야 합니다.", context: DisplayAsset);
+            if (!DisplayShelf.TryGetComponent(out Collider _))
+                return new ValidationError("전시대에 Collider가 있어야 합니다.", context: DisplayShelf);
+            
+            if (!DisplayAsset) return Error("전시 에셋 오브젝트를 설정해주세요.");
+            if (!DisplayShelf) return Error("전시대 오브젝트를 설정해주세요.");
+            
             if (string.IsNullOrWhiteSpace(TargetUrl)) return Error("targetUrl이 비어있습니다.");
             
             return base.ValidationCheck();
